1bb9ee0894bfafc9eeb5fea28c67f3da641e146a
Lakhwinder <lakhwinder32105@gmail.com>
2026-03-31 20:14:30 +0530

    Clean production-ready Django setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cognix.wsgi
---
+++ main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,"index.html")
+def about(request):
+    return render(request, 'about.html')
+def chat(request):
+    return render(request, 'chat.html')
+def gamezone(request):
+    return render(request, 'gamezone.html')
+
+def tools(request):
+    return render(request, 'tools.html')
+
+def login(request):
+    return render(request, 'login.html')
